d495ff08d2f6b7b4a19a6f94bd2d79cada9ad9e3
shorab.rustam <shorab.rustam@mpokket.com>
2024-07-30 15:27:10 +0530

    Hello World Controller

---
+++ src/main/java/com/example/databases/HelloWorld.java
@@ -0,0 +1,13 @@
+package com.example.databases;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorld
+{
+    @GetMapping("/hello")
+    public String helloWorld(){
+        return "Hello World!, Welcome to learn new framework Spring Boot";
+    }
+}
