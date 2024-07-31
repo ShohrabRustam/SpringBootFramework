0ae40504c8741ed9ad652c998f7c661e28e51c99
shorab.rustam <shorab.rustam@mpokket.com>
2024-07-31 12:23:44 +0530

    hello world app

--- src/main/java/com/example/databases/HelloWorld.java
+++ src/main/java/com/example/databases/HelloWorld.java
@@ -1,6 +1,8 @@
 package com.example.databases;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -9,5 +11,10 @@ public class HelloWorld
     @GetMapping("/hello")
     public String helloWorld(){
         return "Hello World!, Welcome to learn new framework Spring Boot";
+    }
+
+    @PostMapping("/hello")
+    public String helloPost(@RequestBody String name){
+        return "Hello! "+ name;
     }
 }
