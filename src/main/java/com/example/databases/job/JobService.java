1e937a017c45d6fb1d95fc3a95f10f3737c72250
shorab.rustam <shorab.rustam@mpokket.com>
2024-08-07 10:53:47 +0530

    define function in interface deleteJobById

--- src/main/java/com/example/databases/job/JobService.java
+++ src/main/java/com/example/databases/job/JobService.java
@@ -1,11 +1,11 @@
 package com.example.databases.job;
 
-import org.jvnet.hk2.annotations.Service;
-
 import java.util.List;
 
 public interface JobService {
     List<Job> findAll();
     void createJob(Job job);
     Job findJobById(Long id);
+    boolean deleteJobById(Long id);
+
 }
