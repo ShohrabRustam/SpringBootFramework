ebdab824471007952c99be6e79bee48b7f895d24
shorab.rustam <shorab.rustam@mpokket.com>
2024-08-07 12:48:48 +0530

    curd operation with basic structure of spring boot has been completed.

--- src/main/java/com/example/databases/DatabasesApplication.java
+++ src/main/java/com/example/databases/DatabasesApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DatabasesApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(DatabasesApplication.class, args);
 	}
 
