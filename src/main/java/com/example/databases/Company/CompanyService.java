112e5ca8f9a788ce007b4d9ebbac0550d536ccfe
shorab.rustam <shorab.rustam@mpokket.com>
2024-08-12 12:32:50 +0530

    interface method for company

--- src/main/java/com/example/databases/Company/CompanyService.java
+++ src/main/java/com/example/databases/Company/CompanyService.java
@@ -4,5 +4,8 @@ import java.util.List;
 
 public interface CompanyService {
     List<Company> getAllCompanies();
+    Company findCompanyById(Long id);
+    void crateCompany(Company company);
+    boolean updateCompany(Long id, Company company);
 
 }
