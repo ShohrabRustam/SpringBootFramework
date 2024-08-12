3a1746c942fefac0902124ec864c28ab2c7e1f89
shorab.rustam <shorab.rustam@mpokket.com>
2024-08-12 10:30:09 +0530

    remove get and set jobs

--- src/main/java/com/example/databases/Company/Company.java
+++ src/main/java/com/example/databases/Company/Company.java
@@ -22,13 +22,6 @@ public class Company {
     public Company() {
     }
 
-    public List<Job> getJobs() {
-        return jobs;
-    }
-
-    public void setJobs(List<Job> jobs) {
-        this.jobs = jobs;
-    }
 
     public String getDecription() {
         return decription;
