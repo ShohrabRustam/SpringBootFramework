ee6f877398097ef6a5f2b5912c5aab2dfd988fb2
shorab.rustam <shorab.rustam@mpokket.com>
2024-08-12 12:34:08 +0530

    company method implementation

--- src/main/java/com/example/databases/Company/Implementation/CompanyServiceImpl.java
+++ src/main/java/com/example/databases/Company/Implementation/CompanyServiceImpl.java
@@ -3,9 +3,11 @@ package com.example.databases.Company.Implementation;
 import com.example.databases.Company.Company;
 import com.example.databases.Company.CompanyRepository;
 import com.example.databases.Company.CompanyService;
-
+import org.springframework.stereotype.Service;
 import java.util.List;
+import java.util.Optional;
 
+@Service
 public class CompanyServiceImpl implements CompanyService {
 
     private CompanyRepository companyRepository;
@@ -17,5 +19,28 @@ public class CompanyServiceImpl implements CompanyService {
     @Override
     public List<Company> getAllCompanies() {
         return companyRepository.findAll();
+    }
+
+    @Override
+    public Company findCompanyById(Long id) {
+        return companyRepository.findById(id).orElse(null);
+    }
+
+    @Override
+    public void crateCompany(Company company) {
+        companyRepository.save(company);
+    }
+
+    @Override
+    public boolean updateCompany(Long id, Company updateCompany) {
+        Optional<Company> companyOptional = companyRepository.findById(id);
+        if (companyOptional.isPresent()){
+            Company company = companyOptional.get();
+            company.setDecription(updateCompany.getDecription());
+            company.setName(updateCompany.getName());
+            companyRepository.save(company);
+            return true;
+        }
+        return false;
     }
 }
