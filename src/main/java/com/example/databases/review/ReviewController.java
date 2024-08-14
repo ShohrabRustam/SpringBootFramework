2005081d58177a4d5878ca306ae5f5fc6abaf996
shorab.rustam <shorab.rustam@mpokket.com>
2024-08-14 11:06:43 +0530

    review Controller

---
+++ src/main/java/com/example/databases/review/ReviewController.java
@@ -0,0 +1,8 @@
+package com.example.databases.review;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ReviewController {
+}
