d0fb33e97ca79963663adcf40e45d5d5c91e8815
shorab.rustam <shorab.rustam@mpokket.com>
2024-08-14 12:29:58 +0530

    Review Entity

--- src/main/java/com/example/databases/review/Review.java
+++ src/main/java/com/example/databases/review/Review.java
@@ -11,6 +11,50 @@ public class Review {
     @Id
     @GeneratedValue(strategy = GenerationType.SEQUENCE)
     private Long id;
+    private String title;
     private String discription;
+    private double rating;
 
+    @OneToMany
+    private Company company;
+
+    public Company getCompany() {
+        return company;
+    }
+
+    public void setCompany(Company company) {
+        this.company = company;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDiscription() {
+        return discription;
+    }
+
+    public void setDiscription(String discription) {
+        this.discription = discription;
+    }
+
+    public double getRating() {
+        return rating;
+    }
+
+    public void setRating(double rating) {
+        this.rating = rating;
+    }
 }
